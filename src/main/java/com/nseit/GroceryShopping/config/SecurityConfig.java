c94bef547038f4f6fe583ebe8a5221b7195cd322
JeevithaVijayakumarN <jeevithav0710@gmail.com>
2022-09-15 15:06:18 +0530

    Back end commit

--- src/main/java/com/nseit/GroceryShopping/config/SecurityConfig.java
+++ src/main/java/com/nseit/GroceryShopping/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
